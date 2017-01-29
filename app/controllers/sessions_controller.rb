a47356f6fcd5bd34cad43e1de66a7013aabf29ef
Dean Palm <dean451@gmail.com>
2017-01-29 15:06:34 -0600

    the only thing I care about in this commit is fixing the form email field bug label from name to email go me

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,22 @@ class SessionsController < ApplicationController
   end
 
   def create
-    params[:email]
-    params[:password]
+
+    if user = User.authenticate(params[:email], params[:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Welcome back , #{user.name}!"
+      redirect_to user
+    else
+      flash.now[:alert] = "Invalid email/password combination!"
+      render :new
+    end
+
   end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_url, notice: "Now you're signed out"
+  end
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,8 +10,16 @@ class User < ApplicationRecord
                      format: /\A[A-Z0-9]+\z/i,
                      uniqueness: { case_sensitive: false }
 
+
+def self.authenticate(email, password)
+  user = User.find_by(email: email)
+  user && user.authenticate(password)
+end
+
+
+
  def gravatar_id
    Digest::MD5::hexdigest(email.downcase)
  end
- 
+
 end
