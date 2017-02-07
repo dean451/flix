9a3fa5d226ed910b3a157d2d6c7c0f590e9abcd9
Dean Palm <dean451@gmail.com>
2017-02-07 00:36:35 -0600

    genre characterizations, many to many model

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -17,6 +17,9 @@ class Movie < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :fans, through: :favorites, source: :user
+  has_many :critics, through: :reviews, source: :user
+  has_many :characterizations, dependent: :destroy
+  has_many :genres, through: :characterizations
 
 
   def self.released
