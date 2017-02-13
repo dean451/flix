a697d5aa721242becd9a3881cce37ef06d9671af
Dean Palm <dean451@gmail.com>
2017-02-12 19:16:20 -0600

    title helpers and application layout cleanup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,16 @@
 module ApplicationHelper
 
-  
+    def page_title
+      if content_for?(:title)
+        "Flix - #{content_for(:title)}"
+      else
+        "Flix"
+      end
+    end
+
+    def title(title)
+      content_for(:title, title)
+    end
+
 
 end
