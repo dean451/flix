dddc2246e01e64a0e633215e96cdee1f88714e1e
Dean Palm <dean451@gmail.com>
2017-02-13 23:27:32 -0600

    custom urls and route helpers

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,11 @@ Rails.application.routes.draw do
   get "signup" => "users#new"
   resources :users
   root "movies#index"
+
+  get "movies/filter/hits" => "movies#index", scope: "hits"
+  get "movies/filter/flops" => "movies#index", scope: "flops"
+  get 'movies/filter/:scope' => "movies#index", as: :filtered_movies
+
   resources :movies do
     resources :reviews
   end
