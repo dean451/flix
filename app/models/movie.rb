a8bfb252eccfdfed3f9151dc2f0eb4f6dab5ee6a
Dean Palm <dean451@gmail.com>
2017-02-13 22:56:48 -0600

    scopes! powerful indeed... nice to know you can call a scope on a through association...

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -22,17 +22,10 @@ class Movie < ApplicationRecord
   has_many :genres, through: :characterizations
 
 
-  def self.released
-    where("released_on <= ?", Time.now).order("released_on desc")
-  end
-
-  def self.hits
-    where('total_gross >= 300000000').order(total_gross: :desc)
-  end
-
-  def self.flops
-    where('total_gross < 50000000').order(total_gross: :asc)
-  end
+  scope :released, -> { where("released_on <= ?", Time.now).order(released_on: :desc) }
+  scope :hits, -> { released.where('total_gross >= 300000000').order(total_gross: :desc) }
+  scope :flops, -> { released.where('total_gross < 50000000').order(total_gross: :asc) }
+  scope :upcoming, -> {  where("released_on > ?", Time.now).order(released_on: :asc) }
 
   def self.recently_added
     order('created_at desc').limit(3)
