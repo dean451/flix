cd7c38362450a3fe23f99a3bfeeddef190546e7e
Dean Palm <dean451@gmail.com>
2017-02-20 23:09:19 -0600

    method helper for links

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -7,6 +7,12 @@ module MoviesHelper
     end
   end
 
+  def nav_link_to(text, url)
+    classes = ['button']
+    classes << 'active' if current_page?(url)
+    link_to(text, url, class: classes.join(' '))
+  end
+
   def image_for(movie)
     if movie.image_file_name.blank?
       image_tag('placeholder.png')
@@ -19,7 +25,7 @@ module MoviesHelper
     image_tag('trash.png', style: 'border: 0', title: 'Delete Review')
   end
 
-  def format_average_stars(movie)   
+  def format_average_stars(movie)
     if movie.average_stars.nil?
       content_tag(:strong, 'No reviews')
     else
